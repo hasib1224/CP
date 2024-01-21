8aaee0b3013b6e92f68329cc4ade91cf92f74ba9
hasib1224 <hasib1224>
2024-01-21 23:58:15 +0600

    dp stock selling problem2 done

--- CP/75_Placement/src/DP/DynamicProg.java
+++ CP/75_Placement/src/DP/DynamicProg.java
@@ -35,10 +35,50 @@ public class DynamicProg {
         memo[2]=1;
 
         return NthTribonacci(n,memo);
-
     }
 
+    public static int findProfit(int[]prices,int index,int buy, int[][]profitIndex){
+
+        if(index>=prices.length){
+            return 0;
+        }
+        if(profitIndex[index][buy]!=-1){
+            return profitIndex[index][buy];
+        }
+
+        int profit=0;
+        if(buy==1){
+            profit=Math.max(-prices[index]+findProfit(prices,index+1,0,profitIndex),findProfit(prices,index+1,1,profitIndex));
+
+
+        }else{
+            profit=Math.max(prices[index]+findProfit(prices,index+1,1,profitIndex),findProfit(prices,index+1,0,profitIndex));
+        }
+
+        return profitIndex[index][buy]=profit;
+    }
+
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][]profitIndex=new int[n][2];
+        for (int i = 0; i <n; i++) {
+            profitIndex[i][0]=-1;
+            profitIndex[i][1]=-1;
+        }
+        if(prices.length==1){
+            return 0;
+        }
+        return findProfit(prices,0,1,profitIndex);
+    }
+
+
+
+
+
     public  static  void main(String[]args){
-        System.out.println(tribonacci(4));
+//        System.out.println(tribonacci(4));
+
+        int[]prices={7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
     }
 }
